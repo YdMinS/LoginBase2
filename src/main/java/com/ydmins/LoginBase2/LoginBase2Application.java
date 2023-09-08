6832549ea81c035dc17bee496e705d52cfed8a51
ydmins <dr.ydmins@gmail.com>
2023-09-08 15:51:27 +0900

    first commit

---
+++ src/main/java/com/ydmins/LoginBase2/LoginBase2Application.java
@@ -0,0 +1,13 @@
+package com.ydmins.LoginBase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginBase2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydmins/LoginBase2/LoginBase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydmins.LoginBase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBase2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
